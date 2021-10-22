625d7b4054d526689e1b45226e4587d1f7148ac7
DaRealNim <darealnim@gmail.com>
2021-10-22 13:34:03 +0200

    AAAAAAAAAAAAAAAAAA 2

--- dpll.ml
+++ dpll.ml
@@ -3,23 +3,23 @@ open List
 (* fonctions utilitaires *********************************************)
 (* filter_map : ('a -> 'b option) -> 'a list -> 'b list
    disponible depuis la version 4.08.0 de OCaml dans le module List :
-           pour chaque élément de `list', appliquer `filter' :
-                   - si le résultat est `Some e', ajouter `e' au résultat ;
+       pour chaque élément de `list', appliquer `filter' :
+           - si le résultat est `Some e', ajouter `e' au résultat ;
    - si le résultat est `None', ne rien ajouter au résultat.
    Attention, cette implémentation inverse l'ordre de la liste *)
 let filter_map filter list =
-        let rec aux list ret =
-                match list with
+    let rec aux list ret =
+        match list with
     | []   -> ret
     | h::t -> match (filter h) with
       | None   -> aux t ret
       | Some e -> aux t (e::ret)
-        in aux list []
+    in aux list []
 
-        (* print_modele : int list option -> unit
+    (* print_modele : int list option -> unit
    affichage du résultat *)
 let print_modele: int list option -> unit = function
-        | None   -> print_string "UNSAT\n"
+    | None   -> print_string "UNSAT\n"
   | Some modele -> print_string "SAT\n";
      let modele2 = sort (fun i j -> (abs i) - (abs j)) modele in
      List.iter (fun i -> print_int i; print_string " ") modele2;
@@ -35,81 +35,81 @@ let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20
 
 (********************************************************************)
 
-(* simplifie : int -> int list list -> int list list 
+(* simplifie : int -> int list list -> int list list
    applique la simplification de l'ensemble des clauses en mettant
    le littéral i à vrai *)
 
 (*let simplifie i clauses =
-        let rec filter_clause clause ret =
-                match clause with
-                | [] -> ret
-                | lit :: tail ->
-                                if lit = (i * -1) then
-                                        filter_clause tail ret
-                        else
-                                filter_clause tail (lit::ret)
+    let rec filter_clause clause ret =
+        match clause with
+        | [] -> ret
+        | lit :: tail ->
+                if lit = (i * -1) then
+                    filter_clause tail ret
+            else
+                filter_clause tail (lit::ret)
      in
-        let rec filter_cnf clauses ret =
-                match clauses with
-                | [] -> ret
-                | c :: tail ->
-                                if List.mem i c then
-                                        filter_cnf tail ret
-                        else if List.mem (i * -1) c then
-                                filter_cnf tail ((filter_clause c [])::ret)
-                        else
-                                filter_cnf tail (c::ret)
+    let rec filter_cnf clauses ret =
+        match clauses with
+        | [] -> ret
+        | c :: tail ->
+                if List.mem i c then
+                    filter_cnf tail ret
+            else if List.mem (i * -1) c then
+                filter_cnf tail ((filter_clause c [])::ret)
+            else
+                filter_cnf tail (c::ret)
 in
-        filter_cnf clauses []
-;;*) 
+    filter_cnf clauses []
+;;*)
 
 let simplifie i clauses =
-        let filter_neg x =
-                if x = (i * -1) then
-                        None
-                else
-                        Some x
+    let filter_neg x =
+        if x = (i * -1) then
+        None
+        else
+        Some x
 in
-        let rec filter clauses ret =
-                match clauses with
-                | [] -> ret
-                | c :: tail ->
-                                if List.mem i c then
-                                        filter tail ret
-                else
-                        filter tail (c::ret)
-        in
-        let i_negless_clauses = List.map (List.filter_map filter_neg) clauses in
-        filter i_negless_clauses []
+    let rec filter clauses ret =
+        match clauses with
+        | [] -> ret
+        | c :: tail ->
+                if List.mem i c then
+                    filter tail ret
+        else
+            filter tail (c::ret)
+    in
+    let i_negless_clauses = List.map (List.filter_map filter_neg) clauses in
+    filter i_negless_clauses []
 ;;
 
 
 (* let rec filter ret clause =
-        match clause with
+    match clause with
     | [] -> ret
-    | lit :: tail -> 
-                    if lit = i then
-                            []
-                                else if lit = (i * -1) then
-                                        filter ret tail 
+    | lit :: tail ->
+            if lit = i then
+                []
+                else if lit = (i * -1) then
+                    filter ret tail
       else
-              filter (lit::ret) tail        
-        in
-                List.map (filter []);; works but gives out empty clauses, shit. *) 
+          filter (lit::ret) tail
+    in
+        List.map (filter []);; works but gives out empty clauses, shit. *)
 
 (* solveur_split : int list list -> int list -> int list option
    exemple d'utilisation de `simplifie' *)
-(* cette fonction ne doit pas être modifiée, sauf si vous changez 
+(* cette fonction ne doit pas être modifiée, sauf si vous changez
    le type de la fonction simplifie *)
 let rec solveur_split clauses interpretation =
-        (* l'ensemble vide de clauses est satisfiable *)
-        if clauses = [] then Some interpretation else
-                (* un clause vide est insatisfiable *)
-                if mem [] clauses then None else
-                        (* branchement *) 
-                        let l = hd (hd clauses) in
-                        let branche = solveur_split (simplifie l clauses) (l::interpretation) in
-                        match branche with
+    (* l'ensemble vide de clauses est satisfiable *)
+    if clauses = [] then Some interpretation else
+        (* un clause vide est insatisfiable *)
+        if mem [] clauses then None else
+            (* branchement *)
+            let l = hd (hd clauses) in
+            let branche = solveur_split (simplifie l clauses) (l::interpretation) in
+            match branche with
   | None -> solveur_split (simplifie (-l) clauses) ((-l)::interpretation)
   | _    -> branche
 
@@ -124,26 +124,26 @@ let rec solveur_split clauses interpretation =
       le littéral de cette clause unitaire ;
     - sinon, lève une exception `Not_found' *)
 let unitaire clauses =
-        (* à compléter *)
-        0
+    (* à compléter *)
+    0
 
-        (* pur : int list list -> int
+    (* pur : int list list -> int
     - si `clauses' contient au moins un littéral pur, retourne
       ce littéral ;
     - sinon, lève une exception `Failure "pas de littéral pur"' *)
 let pur clauses =
-        (* à compléter *)
-        0
+    (* à compléter *)
+    0
 
-        (* solveur_dpll_rec : int list list -> int list -> int list option *)
+    (* solveur_dpll_rec : int list list -> int list -> int list option *)
 let rec solveur_dpll_rec clauses interpretation =
-        (* à compléter *)
-        None
+    (* à compléter *)
+    None
 
-        (* tests *)
-        (* let () = print_modele (solveur_dpll_rec systeme []) *)
-        (* let () = print_modele (solveur_dpll_rec coloriage []) *)
+    (* tests *)
+    (* let () = print_modele (solveur_dpll_rec systeme []) *)
+    (* let () = print_modele (solveur_dpll_rec coloriage []) *)
 
 let () =
-        let clauses = Dimacs.parse Sys.argv.(1) in
-        print_modele (solveur_dpll_rec clauses [])
+    let clauses = Dimacs.parse Sys.argv.(1) in
+    print_modele (solveur_dpll_rec clauses [])
